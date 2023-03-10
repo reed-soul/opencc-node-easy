2a9efb9e73da32d0d473f212ec293528d6a5390a
陆世强 <lsqup0702@hotmail.com>
2023-03-10 10:56:17 +0800

    feat: 中文简繁体转换，暂时支持js和ts文件转换

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const fs = require("fs");
 const path = require("path");
 const jieba = require("nodejieba");
 const OpenCC = require("opencc");
+const parser = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
 
 // 简体中文转繁体中文的 OpenCC 实例
-const converter = new OpenCC("s2t.json");
+const converter = new OpenCC();
 
 // 遍历目录下的所有文件
 function traverseDirectory(dir) {
@@ -20,7 +22,7 @@ function traverseDirectory(dir) {
     } else if (stat.isFile()) {
       // 判断文件类型
       const extname = path.extname(filePath).toLowerCase();
-      if ([".js", ".ts", ".vue"].includes(extname)) {
+      if ([".js", ".ts"].includes(extname)) {
         // 处理JS文件中的简体中文
         convertFile(filePath);
       }
@@ -33,31 +35,36 @@ function convertFile(filePath) {
   // 读取文件内容
   let content = fs.readFileSync(filePath, "utf-8");
 
-  // 将注释部分替换为占位符
-  content = content.replace(/\/\*[\s\S]*?\*\/|\/\/[^\n\r]*/g, (match) => {
-    const len = match.length;
-    const placeholder = Array(len).fill("*").join("");
-    return placeholder;
+  // 将文件内容解析成AST
+  const ast = parser.parse(content, {
+    sourceType: "module",
+    plugins: ["jsx"],
   });
 
-  // 使用 nodejieba 分词将简体中文转换为繁体中文
-  content = jieba
-    .cut(content)
-    .map((word) => {
-      return converter.convertSync(word);
-    })
-    .join("");
-
-  // 将占位符替换回注释内容
-  content = content.replace(/\*+/g, (match) => {
-    const len = match.length;
-    return Array(len).fill("*").join("");
+  // 遍历AST，并对字符串进行简体中文到繁体中文的转换
+  traverse(ast, {
+    enter(path) {
+      if (path.isStringLiteral()) {
+        const value = path.node.value;
+        const newValue = jieba
+          .cut(value)
+          .map((word) => {
+            return converter.convertSync(word);
+          })
+          .join("");
+        path.node.value = newValue;
+      }
+    },
   });
+
+  // 将转换后的AST重新生成代码
+  const output = require("@babel/core").transformFromAstSync(ast, content);
 
   // 将转换后的内容写回文件
-  fs.writeFileSync(filePath, content);
+  fs.writeFileSync(filePath, output.code);
 }
 
 // 测试代码
 const dirPath = "./demo";
+
 traverseDirectory(dirPath);
